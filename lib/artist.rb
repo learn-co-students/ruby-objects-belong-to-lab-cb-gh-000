aba44debb42346a5a455607005fd7d8bbbdd8005
Brennan Brown <brennan.brown@edu.sait.ca>
2017-08-20 05:16:54 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,4 @@
+# Class simply initializes with 'name' getter and setter methods.
+class Artist
+  attr_accessor :name
+end
---
+++ lib/song.rb
@@ -0,0 +1,5 @@
+# Class initializes with 'title' getter and setter methods, as well as the
+# 'Artist' class getter and setter methods, as it 'belongs to' the 'Song' class.
+class Song
+  attr_accessor :title, :artist
+end
