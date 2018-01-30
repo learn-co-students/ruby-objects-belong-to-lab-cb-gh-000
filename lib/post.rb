c3fd8edd8c016f05a15bdcea39dafb329f924af4
Othman Mashaab <red.fantastic@hotmail.com>
2018-01-30 18:39:53 +0000

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,10 @@
+class Post
+
+  attr_accessor :title, :author
+
+  def inintialize(author,title)
+    @author = author
+    @title = title
+  end
+
+end
---
+++ lib/song.rb
@@ -0,0 +1,10 @@
+class Song
+
+  attr_accessor :title,:artist
+
+  def inintialize(title,artist)
+    @title = title
+    @artist = artist
+  end
+
+end
